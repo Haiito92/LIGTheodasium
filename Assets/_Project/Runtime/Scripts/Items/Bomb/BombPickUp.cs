3d0f19acb8808a3968ef8fe7ee56113fe47e14ec
Antoine Hanna <hannaantoinebusiness@gmail.com>
2024-07-12 15:51:55 +0200

    [ADD] BombPickUp Hook
    Maybe we need to generalize this late on if all or multiple pick up end up having the same kind of code

---
+++ Assets/_Project/Runtime/Scripts/Items/Bomb/BombPickUp.cs
@@ -0,0 +1,12 @@
+namespace _Project.Runtime.Scripts.Items.Bomb
+{
+    public class BombPickUp : ItemPickUp
+    {
+        protected override void PickUp()
+        {
+            //Add to inventory
+            
+            base.PickUp();
+        }
+    }
+}
